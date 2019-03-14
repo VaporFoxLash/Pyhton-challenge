d36602dbe8aa2f281d0c8845cf2f62265996b21b
VaporFoxLash <1426409@students.wits.ac.za>
2019-03-14 21:22:56 +0500

    Fixed the loop problem

--- sp_reg.py
+++ sp_reg.py
@@ -1,13 +1,19 @@
 import os
+import pyaudio
 import speech_recognition as sr
 
 r = sr.Recognizer()
+r.energy_threshold = 4000
+
+print("Say something: ")
 
 with sr.Microphone() as source:
 	print("Say something: ")
 	audio = r.listen(source)
-	try:
-		text = r.recognize_google(audio)
-		print("You said: {}".format(text))
-	except:
-		print("Sorry couldn't recognize what you said")
+
+try:
+    print("You said " + r.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+	print("Sorry couldn't recognize what you said")
