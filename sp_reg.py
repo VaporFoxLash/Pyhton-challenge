53edce20de0ffac06064b1bbab5edf1e8f9d6e42
VaporFoxLash <1426409@students.wits.ac.za>
2019-03-14 21:11:22 +0500

    Simpler speech recognition

---
+++ sp_reg.py
@@ -0,0 +1,13 @@
+import os
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+	print("Say something: ")
+	audio = r.listen(source)
+	try:
+		text = r.recognize_google(audio)
+		print("You said: {}".format(text))
+	except:
+		print("Sorry couldn't recognize what you said")
